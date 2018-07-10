7051a057c628009b3242eb9737519f7e91012afa
lovit <soy.lovit@gmail.com>
2018-07-10 13:44:44 +0900

    Add import path

--- wordvec_infer/__init__.py
+++ wordvec_infer/__init__.py
@@ -3,3 +3,4 @@ from .pmi import infer_pmi
 from .utils import get_process_memory
 from .utils import check_dirs
 from .vectorizer import sents_to_word_contexts_matrix
+from .vectorizer import sents_to_unseen_word_contexts_matrix
