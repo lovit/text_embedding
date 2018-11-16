fc0c470240a8e86df54fccfe5a4a6dd8289bfa88
lovit <soy.lovit@gmail.com>
2018-11-16 16:36:18 +0900

    Commit Doc2VecCorpus

--- wordvec_infer/utils.py
+++ wordvec_infer/utils.py
@@ -14,24 +14,40 @@ def check_dirs(filepath):
         print('created {}'.format(dirname))
 
 class Word2VecCorpus:
-    def __init__(self, path, num_doc=-1):
+    def __init__(self, path, num_doc=-1, verbose_point=-1):
         self.path = path
         self.num_doc = num_doc
+        self.verbose_point = verbose_point
 
     def __iter__(self):
+        vp = self.verbose_point
         with open(self.path, encoding='utf-8') as f:
             for i, doc in enumerate(f):
+                if vp > 0 and i % vp == 0:
+                    print('\riterating corpus ... %d lines' % (i+1), end='', flush=True)
                 if self.num_doc > 0 and i >= self.num_doc:
                     break
                 doc = doc.strip()
                 if not doc:
                     continue
-                self._tokenize(doc)
+                streams = self._tokenize(i, doc)
+                for stream in streams:
+                    yield stream
+            if vp > 0:
+                print('\riterating corpus was done%s' % (' '*20))
 
-    def _tokenize(self, doc):
+    def _tokenize(self, i, doc):
         for sent in doc.split('  '):
             if sent:
-                yield word2vec_tokenizer(sent)
+                yield sent.split()
 
-def word2vec_tokenizer(sent):
-    return sent.split()
+class Doc2VecCorpus(Word2VecCorpus):
+    def _tokenize(self, i, doc):
+        column = doc.split('\t')
+        if len(column) == 1:
+            labels = ['__doc{}__'.format(i)]
+            words = column[0].split()
+        else:
+            labels = column[0].split()
+            words = [word for col in column[1:] for word in col.split()]
+        yield labels, words
