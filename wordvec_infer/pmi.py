b48c0124c4a87b9e009a8244e298e6a1cd0eef59
lovit <soy.lovit@gmail.com>
2018-11-16 15:27:37 +0900

    Refactoring

--- wordvec_infer/pmi.py
+++ wordvec_infer/pmi.py
@@ -8,47 +8,13 @@ def _as_diag(px, alpha):
     px_diag.data[0] = np.asarray([0 if v == 0 else 1 / (v + alpha) for v in px_diag.data[0]])
     return px_diag
 
-def _as_csr_matrix(exp_pmi, min_pmi, verbose):
-    # PPMI using threshold
-    min_exp_pmi = 1 if min_pmi == 0 else np.exp(min_pmi)
-
+def _logarithm_and_ppmi(exp_pmi, min_exp_pmi):
     # because exp_pmi is sparse matrix and type of exp_pmi.data is numpy.ndarray
     indices = np.where(exp_pmi.data > min_exp_pmi)[0]
 
     # apply logarithm
     exp_pmi.data = np.log(exp_pmi.data)
-
     return exp_pmi
-
-def _as_dok_matrix(exp_pmi, min_pmi, verbose):
-    # PPMI using threshold
-    min_exp_pmi = 1 if min_pmi == 0 else np.exp(min_pmi)
-
-    # because exp_pmi is sparse matrix and type of exp_pmi.data is numpy.ndarray
-    indices = np.where(exp_pmi.data > min_exp_pmi)[0]
-
-    pmi_dok = dok_matrix(exp_pmi.shape)
-
-    # prepare data (rows, cols, data)
-    rows, cols = exp_pmi.nonzero()
-    data = exp_pmi.data
-
-    # enumerate function for printing status
-    for _n_idx, idx in enumerate(indices):
-
-        # print current status
-        if verbose and _n_idx % 10000 == 0:
-            print('\rcomputing pmi {:.3} %  mem={} Gb    '.format(
-                100 * _n_idx / indices.shape[0], '%.3f' % get_process_memory())
-                  , flush=True, end='')
-
-        # apply logarithm
-        pmi_dok[rows[idx], cols[idx]] = np.log(data[idx])
-
-    if verbose:
-        print('\rcomputing pmi was done{}'.format(' '*30), flush=True)
-
-    return pmi_dok
 
 def train_pmi(x, py=None, min_pmi=0, alpha=0.0, beta=1, as_csr=True, verbose=False):
     """
@@ -85,9 +51,9 @@ def train_pmi(x, py=None, min_pmi=0, alpha=0.0, beta=1, as_csr=True, verbose=Fal
     assert 0 < beta <= 1
 
     # convert x to probability matrix & marginal probability 
-    px = (x.sum(axis=1) / x.sum()).reshape(-1)
+    px = np.asarray((x.sum(axis=1) / x.sum()).reshape(-1))
     if py is None:
-        py = (x.sum(axis=0) / x.sum()).reshape(-1)
+        py = np.asarray((x.sum(axis=0) / x.sum()).reshape(-1))
     if beta < 1:
         py = py ** beta
     pxy = x / x.sum()
@@ -96,12 +62,14 @@ def train_pmi(x, py=None, min_pmi=0, alpha=0.0, beta=1, as_csr=True, verbose=Fal
     # using scipy.sparse.diags
     # pmi_alpha (x,y) = p(x,y) / ( p(x) x (p(y) + alpha) )
     px_diag = _as_diag(px, 0)
-    py_diag = _as_diag(py, alpha, beta)
+    py_diag = _as_diag(py, alpha)
     exp_pmi = px_diag.dot(pxy).dot(py_diag)
 
-    if as_csr:
-        pmi = _as_csr_matrix(exp_pmi, min_pmi, verbose)
-    else:
-        pmi = _as_dok_matrix(exp_pmi, min_pmi, verbose)
+    # PPMI using threshold
+    min_exp_pmi = 1 if min_pmi == 0 else np.exp(min_pmi)
+    pmi = _logarithm_and_ppmi(exp_pmi, min_exp_pmi)
 
-    return pmi, px, py
+    if as_csr:
+        return pmi, px, py
+    else:
+        return pmi.todok(), px, py
