c5ff5012dbd9c1f94a84b6e4162d7e0482509527
lovit <soy.lovit@gmail.com>
2018-11-19 22:44:41 +0900

    -

--- wordvec_infer/fasttext.py
+++ wordvec_infer/fasttext.py
@@ -11,12 +11,12 @@ def subword_tokenizer(term, min_n=3, max_n=6):
     subwords: list of str
         It contains subword of "<term>".
         '<' and '>' are special symbol that represent begin and end of term.
-    term_: str
-        Term that attached '<' and '>'
+        First element is '<' and '>' attached term
     """
 
     term_ = '<%s>' % term
     len_ = len(term_)
     subwords = [term_[b:b+n] for n in range(min_n, max_n + 1)
                 for b in range(len_ - n + 1)]
-    return subwords, term_
+    subwords.insert(0, term_)
+    return subwords
