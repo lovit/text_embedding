2a1d225d116614aed119c1c1fa1bd7a0810c6a7c
lovit <soy.lovit@gmail.com>
2018-12-10 20:06:28 +0900

    Clarify variable name

--- text_embedding/utils.py
+++ text_embedding/utils.py
@@ -14,12 +14,12 @@ def check_dirs(filepath):
         print('created {}'.format(dirname))
 
 class Word2VecCorpus:
-    def __init__(self, path, num_doc=-1, verbose_point=-1, lowercase=True, sent_delimiter='\n'):
+    def __init__(self, path, num_doc=-1, verbose_point=-1, lowercase=True, sentence_separator='\n'):
         self.path = path
         self.num_doc = num_doc
         self.verbose_point = verbose_point
         self.lowercase = lowercase
-        self.sent_delimiter = sent_delimiter
+        self.sentence_separator = sentence_separator
         self._len = 0
 
     def __iter__(self):
@@ -43,7 +43,7 @@ class Word2VecCorpus:
             self._len = i + 1
 
     def _tokenize(self, i, doc):
-        return [sent.split() for sent in doc.split(self.sent_delimiter) if sent]
+        return [sent.split() for sent in doc.split(self.sentence_separator) if sent]
 
     def __len__(self):
         if self._len == 0:
@@ -54,20 +54,20 @@ class Word2VecCorpus:
         return self._len
 
 class Doc2VecCorpus(Word2VecCorpus):
-    def __init__(self, path, num_doc=-1, verbose_point=-1, lowercase=True, yield_label=True, sent_delimiter='\n'):
-        super().__init__(path, num_doc, verbose_point, lowercase, sent_delimiter)
+    def __init__(self, path, num_doc=-1, verbose_point=-1, lowercase=True, yield_label=True, sentence_separator='\n'):
+        super().__init__(path, num_doc, verbose_point, lowercase, sentence_separator)
         self.yield_label = yield_label
 
     def _tokenize(self, i, doc):
         column = doc.split('\t')
         if len(column) == 1:
             labels = ['__doc{}__'.format(i)]
-            sents = column[0].split(self.sent_delimiter)
+            sents = column[0].split(self.sentence_separator)
         else:
             labels = column[0].split()
             sents = []
             for col in column[1:]:
-                sents += col.split(self.sent_delimiter)
+                sents += col.split(self.sentence_separator)
         if self.yield_label:
             return [(labels, tuple(sent.split())) for sent in sents if sent]
         else:
